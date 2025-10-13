878efa56ed96f5c74bbac55c857d21dba4d58d6c
stefanfasano <stefanfasano@gmail.com>
2025-10-13 08:42:38 -0500

    added some extra pid dependancies and a testbed class

---
+++ h4-sdk-core/apps/h4IMUTestbed.cpp
@@ -0,0 +1,63 @@
+//
+// Created by srfas on 10/9/2025.
+//
+
+#include <ethercatcpp/core.h>
+#include <pid/synchro.h>
+#include <pid/real_time.h>
+#include <pid/signal_manager.h>
+#include <pid/log.h>
+#include <chrono>
+
+using std::chrono_literals::operator ""ms;
+
+int main(int argc, char* argv[]) {
+    auto memory_locker = pid::make_current_thread_real_time();
+
+    // Master creation
+    ethercatcpp::Master master;
+
+    // Adding network interface (replace by adequet id)
+    master.set_primary_interface("eth0");
+
+    // Device definition
+    ethercatcpp::EK1100 EK1100;
+    ethercatcpp::EL5101 EL5101;
+
+    // Linking device to bus in hardware order !!
+    master.add(EK1100);
+    master.add(EL5101);
+
+    // Initilize the network master
+    master.init();
+
+    bool stop = false;
+    pid::SignalManager::add(pid::SignalManager::Interrupt, "SigInt stop",
+                            [&stop]() { stop = true; });
+
+    //const auto period = std::chrono::duration<double>(control_period);
+    pid::Period period(1ms);
+
+    pid_log << "Starting periodic loop" << pid::endl;
+    while (not stop) {
+
+        // SET config => set the command buffer
+        // EL5101.enableenable_Latch_On(ethercatcpp::EL5101::latch_pin_C, false);
+        // EL5101.enable_Latch_On(ethercatcpp::EL5101::latch_pin_ext_pos, false);
+        // EL5101.enable_Latch_On(ethercatcpp::EL5101::latch_pin_ext_neg, false);
+        // EL5101.enable_Counter_offset(false);
+        // EL5101.set_Counter_Offset_Value(0);
+
+        // If cycle is correct read data
+        if (master.next_cycle()) {
+            pid_log << "EL5101 state:" << pid::endl;
+            EL5101.print();
+        }
+
+        period.sleep();
+    }
+
+    //Cleanly terminate communication
+    pid::SignalManager::remove(pid::SignalManager::Interrupt, "SigInt stop");
+    master.end();
+}
--- h4-sdk-core/src/lib.cpp
+++ h4-sdk-core/src/lib.cpp
@@ -2,9 +2,9 @@
 // Created by srfas on 10/9/2025.
 //
 
-#include <lib.h>
-
+#include <../include/lib.h>
 #include<pid/rpath.h>
+#include <ethercatcpp/core.h>
 
 std::string get_Absolute_Path(const std::string& rel_path){
     return (PID_PATH(rel_path));
