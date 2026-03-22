c1a8cafb34dcbf4407bcddddedc702cf7b1e2a67
stefanfasano <stefanfasano@gmail.com>
2026-03-22 15:00:11 -0500

    added buffer offset holder

---
+++ soem-cpp-core/include/BufferOffsetHolder.h
@@ -0,0 +1,47 @@
+//
+// Created by srfas on 3/22/2026.
+//
+
+#ifndef SOEM_CPP_BUFFEROFFSETHOLDER_H
+#define SOEM_CPP_BUFFEROFFSETHOLDER_H
+
+#include <cstdint>
+
+class BufferOffsetHolder
+{
+    const int bitSize; //The entire bit size of a slave's output or input buffer, plus all bits before that in the master buffer
+    int byteOffset{0};
+    int bitOffset{0};
+
+public:
+    BufferOffsetHolder(uint32_t byteOffset, uint8_t bitOffset, uint16_t bitSize);
+
+    /**
+    * Align the offsets on a byte boundary if bytes > 0
+    *
+    *
+    * @param bytes
+    * @param bits
+    */
+    void align(int bytes, int bits);
+
+    /**
+    * Increase the offsets, wrapping bits into bytes.
+    *
+    * @param bytes
+    * @param bits
+    * @return true if space is available, false if there is not enough space for the increase
+    */
+    bool increase(int bytes, int bits);
+
+    const int getByteOffset() const;
+
+    const int getBitOffset() const;
+
+    /**
+    * @return the number of bits available in this buffer
+    */
+    int getAvailableBits() const;
+};
+
+#endif //SOEM_CPP_BUFFEROFFSETHOLDER_H
